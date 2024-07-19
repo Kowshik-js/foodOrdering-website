dd3c18048cd03d3a65c5da8370288a089d0ce144
Kowshik-js <142918527+Kowshik-js@users.noreply.github.com>
2024-07-19 20:19:46 +0530

    first commir

---
+++ dist/index.7c0ccee6.js
@@ -0,0 +1,21 @@
+const parent = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child"
+    }, [
+        React.createElement("h1", {}, "kowshik"),
+        React.createElement("h2", {}, "kowshik")
+    ]),
+    React.createElement("div", {
+        id: "child2"
+    }, [
+        React.createElement("h1", {}, "kowshik"),
+        React.createElement("h2", {}, "kowshik")
+    ])
+]);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=index.7c0ccee6.js.map
